7a07761f45dcf8cadba2a7d67a69ba26ecc91fa8
John Williamson <johnhw@gmail.com>
2023-10-02 13:10:47 +0100

    Added per-unit bots

--- makebot.py
+++ makebot.py
@@ -2,27 +2,51 @@ from pathlib import Path
 import sys
 
 
-if len(sys.argv)>1:
-    template_file = sys.argv[1]
-else:
-    template_file = 'makebot/TEMPLATE.txt'
+def sub(text, components):
+    """Replace the components in the text."""
+    for key, value in components.items():
+        text = text.replace(f"{{{key}}}", f"{key.upper().replace('_', ' ')}:\n" + value)
+    return text
 
-# read the components 
+
+def write(text, fname):
+    """Write the text to the file."""
+    with open(fname, "w", encoding="utf8") as f:
+        f.write(text)
+    print(f"Wrote {fname}")
+
+
+mode = "normal"
+if len(sys.argv) > 1:
+    mode = sys.argv[1]
+
+if mode == "normal":
+    template_file = "templates/TEMPLATE.txt"
+else:
+    template_file = "templates/TEMPLATE_PER_UNIT.txt"
+
+# read the components
 components = {}
-for file in Path('makebot').glob('*.txt'):
-    with open(file, 'r', encoding="utf8") as f:
+for file in Path("components").glob("*.txt"):
+    with open(file, "r", encoding="utf8") as f:
         components[file.stem] = f.read()
 
-
 # read the template
-with open(template_file, 'r', encoding="utf8") as f:
+with open(template_file, "r", encoding="utf8") as f:
     template = f.read()
 
-# replace the components
-for key, value in components.items():
-    template = template.replace(f'{{{key}}}', f"{key.upper().replace('_', ' ')}:\n"+value)
+if mode != "normal":
+    # replace the components
+    for unit in range(1, 10):
+        unit_name = "unit_" + str(unit)
+        if unit_name in components:
+            components["units"] = components[unit_name]
+            template = sub(template, components)
+            # write the output
+            write(template, f"units/DFHBOT_UNIT_{unit}.txt")
 
-# write the output
-with open('DFHBOT.txt', 'w', encoding="utf8") as f:
-    f.write(template)
-
+else:
+    # replace the components
+    template = sub(template, components)
+    # write the output
+    write(template, "DFHBOT.txt")
